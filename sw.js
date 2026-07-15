93364f0d12b341be83db32ab101fe3967e1c906f
Oden225 <131435571+Oden225@users.noreply.github.com>
2026-07-15 04:13:29 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "visitemed-v3";
+const CACHE = "visitemed-v4";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
